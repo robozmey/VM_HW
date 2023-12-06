3934ab3d603586b44ef1b7c7f93e15e32b0195f8
Vladimir Rachkin <vova@kpnn.ru>
2023-12-06 22:37:25 +0300

    HW1 release 3

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -16,7 +16,7 @@ const int  LINE_SIZE_ITERATIONS = 10;
 
 const int MEASURE_N = 1 << 20;
 
-const double ASSOC_THRESHOLD = 1.5;
+const double ASSOC_THRESHOLD = 1.8;
 const double LINE_SIZE_THRESHOLD = 1.2;
 
 uint32_t a[SIZE] alignas(8192);
